83b1bd943646d7363296d8c2b4858c26dbfc0e24
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-03-27 16:56:23 +0100

    updated neovim install script to use dotfile installer

--- installs/Neovim.ps1
+++ installs/Neovim.ps1
@@ -1,38 +1,24 @@
 <#
 .SYNOPSIS
-    Neovim installation script
+Neovim installation script
 .DESCRIPTION
-    Installs Neovim via winget and installs the dotfiles
+Installs Neovim via winget and installs the dotfiles
 .AUTHOR
-    Dubsky
+Dubsky
 .REPOSITORY
-    https://github.com/dubskysteam/WinFlux
+https://github.com/dubskysteam/WinFlux
 .VERSION
-    1.0.0
+1.0.0
 #>
 
-try {
-    if (-not (Get-Command nvim -ErrorAction SilentlyContinue)) {
-        winget install -e --id Neovim.Neovim --silent
-        Write-Output "Neovim installed successfully"
-        Write-Output "Trying to install dotfiles"
-        $repoUrl = "https://api.github.com/repos/DubskySteam/.dotfiles/tree/main/nvim/.config/nvim"
-        $localPath = "$env:LOCALAPPDATA\nvim"
+Import-Module "$PSScriptRoot/../modules/DotfilesInstaller.psm1" -Force
 
-        $files = (Invoke-RestMethod -Uri $repoUrl) | Where-Object { $_.type -eq "file" }
+if (-not (Get-Command nvim -ErrorAction SilentlyContinue)) {
+    winget install Neovim.Neovim --accept-source-agreements
+}
 
-        foreach ($file in $files) {
-            $dest = Join-Path $localPath $file.path.Replace("nvim/", "")
-            $null = New-Item -Path (Split-Path $dest -Parent) -ItemType Directory -Force
-            Invoke-WebRequest -Uri $file.download_url -OutFile $dest
-        }
-        Write-Output "dotfiles installed"
-    } else {
-        Write-Output "Neovim already installed"
-    }
-    <#Wait 2seconds#>
-    Start-Sleep -Seconds 2
-}
-catch {
-    throw "Neovim installation failed: $_"
-}
+Install-Dotfiles -GitHubUser "DubskySteam" `
+                 -RepoName ".dotfiles" `
+                 -SourcePath "nvim/.config/nvim" `
+                 -LocalPath "$env:LOCALAPPDATA\nvim" `
+                 -CleanBeforeInstall
