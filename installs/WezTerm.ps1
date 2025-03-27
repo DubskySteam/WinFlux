c6e2a4edf081b529ff621c35d947d426da02ace0
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-03-27 17:42:35 +0100

    added wezterm

---
+++ installs/WezTerm.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS
+Wezterm installation script
+.DESCRIPTION
+Installs Wezterm via winget and installs the dotfiles
+.AUTHOR
+Dubsky
+.REPOSITORY
+https://github.com/dubskysteam/WinFlux
+.VERSION
+1.0.0
+#>
+
+Import-Module "$PSScriptRoot/../modules/DotfilesInstaller.psm1" -Force
+
+if (-not (Get-Command wezterm -ErrorAction SilentlyContinue)) {
+    winget install --id=wez.wezterm --silent --accept-source-agreements
+}
+
+Install-Dotfiles -GitHubUser "DubskySteam" `
+                 -RepoName ".dotfiles" `
+                 -SourcePath "wezterm/.config/wezterm" `
+                 -LocalPath "$HOME" `
+                 -CleanBeforeInstall
